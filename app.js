02e1abe79e0d3b2569cd453d9078084e2b535983
aa.lvov@i-teco.ru <aa.lvov@i-teco.ru>
2025-02-26 12:43:37 +0300

    тест гита

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 8088
 
 app.get("/", (req, res) => {
-    res.send("<h2>Привет Express!</h2>");
+    res.send("<h2>Тестовое изменение</h2>");
 });
 
 app.listen(port, () => {
